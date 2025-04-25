713327130cac9f79229d9ca17fc812928c7a3cc7
niaz-111 <niaz0218ofc@gmail.com>
2025-04-25 19:44:16 +0600

    added gemini flash 1.5

--- note/llm_instance.py
+++ note/llm_instance.py
@@ -1,9 +1,21 @@
 from langchain_ollama.llms import OllamaLLM
 from langchain_ollama import OllamaEmbeddings
+import os
+import google.generativeai as genai
+from langchain_google_genai import ChatGoogleGenerativeAI
 
 #OLLAMA_URL = "http://107.109.214.240:11434/api/generate"
 OLLAMA_URL = "http://127.0.0.1:11434"
 
-print("LLM model is initializing")
+#deepseek v2 local
 llm = OllamaLLM(model="deepseek-v2", temperature=0, base_url=OLLAMA_URL)
+
+#gemini flash 1.5 API key
+llm = ChatGoogleGenerativeAI(
+    model="models/gemini-1.5-flash-latest",  
+    google_api_key="AIzaSyDRFIqQrekDkbULaXo8TbIrdZSz7P7TPMk",     
+    temperature=0.3,
+    convert_system_message_to_human=True
+)
+
 embedding_model = OllamaEmbeddings(model="nomic-embed-text", base_url=OLLAMA_URL)
